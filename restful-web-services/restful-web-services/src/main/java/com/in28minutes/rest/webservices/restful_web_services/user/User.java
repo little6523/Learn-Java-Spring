be88b44598ee70c86bcb0edce246e1aabb6f0438
LeeHyeonjun <little6523@naver.com>
2024-11-14 02:10:50 +0900

    삭제된 실습파일 복구

--- restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -1,12 +1,38 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
-import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity(name = "user_details")
 public class User {
 
+    protected User() {
+
+    }
+
+    @Id
+    @GeneratedValue
     private Integer id;
+
+    @Size(min=2, message = "Name should have atleast 2 characters")
+//    @JsonProperty("user_name")
     private String name;
+
+    @Past(message = "Birth Date should be in the past")
+//    @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<Post> posts;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
@@ -45,5 +71,13 @@ public class User {
                 ", name='" + name + '\'' +
                 ", birthDate=" + birthDate +
                 '}';
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
--- restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,13 +1,14 @@
 package com.in28minutes.rest.webservices.restful_web_services.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import java.net.URI;
 import java.util.List;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -24,14 +25,26 @@ public class UserResource {
         return service.findAll();
     }
 
+    //http://localhost:8080/users
     @GetMapping("/users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException("id:"+id);
         }
-        return user;
+
+        EntityModel<User> entityModel = EntityModel.of(user);
+
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+
+        return entityModel;
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        service.deleteById(id);
     }
 
     //Post /users
