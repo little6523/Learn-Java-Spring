e494fdd376eb2deb5ceb2b64dfe188fe4582258c
LeeHyeonjun <little6523@naver.com>
2024-12-01 08:23:08 +0900

    24.12.01 Java 함수형 프로그래밍 실습

---
+++ 91-aws/functional-programming-with-java/src/programming/FP01Structured.java
@@ -0,0 +1,27 @@
+package programming;
+
+import java.util.List;
+
+public class FP01Structured {
+
+  public static void main(String[] args) {
+
+    List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+//    printAllNumberInListStructured(numbers);
+    printEvenNumberInListStructured(numbers);
+  }
+
+  private static void printAllNumberInListStructured(List<Integer> numbers) {
+    for (int number : numbers) {
+      System.out.println(number);
+    }
+  }
+
+  private static void printEvenNumberInListStructured(List<Integer> numbers) {
+    for (int number : numbers) {
+      if (number % 2 == 0) {
+        System.out.println(number);
+      }
+    }
+  }
+}
